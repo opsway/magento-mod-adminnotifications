2577a28ead7d793a59c3455f0d95b85b608f4540
Andriy Samilyak <samilyak@gmail.com>
2014-03-06 16:20:49 +0200

    Blocking pop-up

--- app/code/local/Mage/BetterAdminNotifications/Block/Notification/Window.php
+++ app/code/local/Mage/BetterAdminNotifications/Block/Notification/Window.php
@@ -4,7 +4,7 @@ class Mage_BetterAdminNotifications_Block_Notification_Window extends Mage_Admin
 {
     public function canShow()
     {
-        if (!Mage::getStoreConfig('better_admin_notifications/general/enabled'))
+        if (!Mage::getStoreConfig('general/better_admin_notifications/enabled'))
         {
             return parent::canShow();
         }
